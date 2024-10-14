de8e2b725a15db5eceae5fb31874530407433ed7
Winston Trinh <winstontrinh4@gmail.com>
2024-10-14 13:08:48 -0700

    Fix import error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 import { HackathonCard } from "@/components/hackathon-card";
 import BlurFade from "@/components/magicui/blur-fade";
 import BlurFadeText from "@/components/magicui/blur-fade-text";
-import WordFadeIn from "@/components/magicui/word-fade-text";
 import { ProjectCard } from "@/components/project-card";
 import { ResumeCard } from "@/components/resume-card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
