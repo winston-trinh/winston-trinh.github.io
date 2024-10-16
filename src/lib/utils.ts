8aff9f68ca76c6b9a2b5a94e4af87c9d9b6e079f
Winston Trinh <winstontrinh4@gmail.com>
2024-10-15 17:47:49 -0700

    Set date to Pacific time

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,19 +5,29 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function formatDate(date: string) {
-  let currentDate = new Date().getTime();
-  if (!date.includes("T")) {
-    date = `${date}T00:00:00`;
+export function formatDate(dateString: string) {
+  if (!dateString.includes("T")) {
+    dateString = `${dateString}T00:00:00`;
   }
-  let targetDate = new Date(date).getTime();
-  let timeDifference = Math.abs(currentDate - targetDate);
-  let daysAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
 
-  let fullDate = new Date(date).toLocaleString("en-us", {
-    month: "long",
-    day: "numeric",
-    year: "numeric",
+  const timeZone = 'America/Los_Angeles';
+
+  const targetDate = new Date(dateString);
+  const targetDateInPacific = new Date(
+    targetDate.toLocaleString('en-US', { timeZone })
+  );
+
+  const now = new Date();
+  const nowInPacific = new Date(
+    now.toLocaleString('en-US', { timeZone })
+  );
+  const timeDifference = nowInPacific.getTime() - targetDateInPacific.getTime();
+  const daysAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  const fullDate = targetDateInPacific.toLocaleString('en-US', {
+    timeZone,
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
   });
 
   if (daysAgo < 1) {
