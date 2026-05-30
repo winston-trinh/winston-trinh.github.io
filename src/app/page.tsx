4448bd4bc11ff3ce9589da4de1e701ea45be57ba
Winston Trinh <112506321+winston-trinh@users.noreply.github.com>
2026-05-29 22:50:38 -0400

    Update email

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -384,7 +384,7 @@ export default function Page() {
               <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                 Want to chat? Just shoot me a {" "}
                 <Link
-                  href={"mailto:wntrinh@usc.edu"}
+                  href={"mailto:winstontrinh4@gmail.com?subject=Portfolio Inquiry"}
                   className="text-blue-500 hover:underline"
                 >
                   email
