9526e54989993494f033d7285ccf1aaf9f9af6ee
Winston Trinh <112506321+winston-trinh@users.noreply.github.com>
2026-06-05 19:41:29 -0400

    Resume frame

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Resume", // This will populate your template in the browser tab
+};
+
+export default function ResumePage() {
+  return (
+    <div className="h-screen w-full">
+      <iframe
+        src="/resume_winston_trinh.pdf"
+        className="h-full w-full border-0"
+        title="Resume"
+      />
+    </div>
+  );
+}
